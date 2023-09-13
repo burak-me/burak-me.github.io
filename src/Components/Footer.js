ffd702f0e48c344f01254b95d9a0174e1ec91c8a
burak-me <144389779+burak-me@users.noreply.github.com>
2023-09-13 13:51:37 +0300

    5

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,7 +12,8 @@ const Footer = () => {
   return (
     <footer>
       <h4>Developed by Burak Sahin</h4>
-      <h4>Copyright &copy; 2024 BS</h4>
+      <h4>Copyright &copy; 2024 <a href="https://burak.me" target="_blank"
+                rel="noreferrer" ><b>burak.me</b></a></h4>
       <div className='footerLinks'>
         <a href="https://github.com/burak-me" target='_blank'><FaGithub/></a>
         <a href="https://twitter.com/" target='_blank'><AiOutlineTwitter/></a>
